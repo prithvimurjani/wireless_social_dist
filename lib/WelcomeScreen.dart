728d432fb7cbbec20a231fa5320aefdf9e0c41f3
prithvimurjani <prithvimurjani@gmail.com>
2021-04-19 22:15:40 +0530

    Welcome screen UI

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wireless_social_dist/constants.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = "WelcomeScreen";
@@ -9,6 +10,56 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Smart Social Distancing Device',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'ECE 3040',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'wireless_images/wireless_image.png',
+                scale: 1,
+              ),
+              MaterialButton(
+                color: Colors.black,
+                child: Text(
+                  "Tap to proceed >>>",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                highlightColor: kPinkColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kBlueColor = Color(0xFFA2DAD3);
+const kPinkColor = Color(0xFFF76F70);
