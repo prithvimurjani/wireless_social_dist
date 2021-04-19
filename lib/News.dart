39b52c157aa1828ae39a3d2e517e37edc0fa4870
prithvimurjani <prithvimurjani@gmail.com>
2021-04-20 00:01:09 +0530

    Release apk test

--- lib/News.dart
+++ lib/News.dart
@@ -144,6 +144,7 @@ class _NewsState extends State<News> {
                           onPressed: () {
                             setState(() {
                               this.widget;
+                              
                             });
                           },
                         ),
